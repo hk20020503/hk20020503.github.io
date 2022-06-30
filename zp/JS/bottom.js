f57ba428297e9ae78b8bf2142918c925b7fae3c2
Huang kai <3068404410@qq.com>
2022-06-30 23:39:41 +0800

    first commit

---
+++ zp/JS/bottom.js
@@ -0,0 +1,4 @@
+document.writeln("<div class=\'dibu\'>");
+document.writeln("                <p class=\'db1\'>KAI<span>′S</span>博客</p>");
+document.writeln("                <p class=\'db2\'>联系我们:3068404410@qq.com</p>");
+document.writeln("            </div>");
